0a6567b1d46d06bceeb20faa06087c2974f3ac7a
MOISES AARON BUSTILLOS SANDOVAL <a361352@uach.mx>
2024-05-21 12:46:45 -0600

    se suben el archivo de la practica

---
+++ app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((request, response)=>{
+    const file = request.url == '/' ? './WWW/index.html': `./WWW/${request.url}`;
+    fs.readFile(file, (err, data)=>{
+        if(err){
+            response.writeHead(404, {"Content-Type":"text/plain"});
+            response.write("Not found");
+            response.end();
+            //mandar 404
+        }else{
+            const extension = request.url.split('.').pop();
+            switch(extension) {
+                case 'txt':
+                    response.writeHead(200, {"Content-Type":"text/plain"});
+                break;
+                case 'html':
+                    response.writeHead(200, {"Content-Type":"text/html"});
+                break;
+                case 'png':
+                    response.writeHead(200, {"Content-Type":"image/png"});
+                break;
+            }
+
+            response.write(data);
+            response.end();
+        }
+    });
+
+}).listen(4444);
